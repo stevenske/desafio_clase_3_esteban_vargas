17379c91ae44b5746f0f1ae018e86a03824aee68
esteban vargas <esteban.var@outlook.com>
2022-09-06 22:24:12 -0300

    add counter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import NavBar from './components/navBar/NavBar';
 import ItemListContainer from './components/itemListContainer/ItemListContainer';
+import Counter from './components/counter/Counter';
 function App() {
+// los tastes seria el "stock"
+
+  const tastes = 4
   return (
     <div className="App">
       <NavBar/>
       <ItemListContainer greeting='bienvenido a helados a tu gusto!! '/>
+      <Counter tastes = {tastes}/>
     </div>
   );
 }
